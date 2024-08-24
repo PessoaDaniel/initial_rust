85d30615f517008230f1b439ecd58f103393ccc8
daniel <danielpssoa432@gmail.com>
2024-08-24 17:36:04 -0300

    immutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
+    immutable_variables();
+    mutable_variables();
+}
+
+fn immutable_variables() {
+    println!("[Immutable value]");
+    let x = 5;
+    println!("The value of x is: {x}");
+    // x = 6;
+    println!("The value of x is: {x}");
+}
+
+fn mutable_variables() {
+    println!("[Mutable value]");
+
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
